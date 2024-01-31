95806f26e8f59dacfbc5de44c60133ff1cd90015
bryokn <bryokn@gmail.com>
2024-01-31 16:39:36 +0300

    Passed test

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,20 +3,81 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
-  function handleAddFood() {
+  const foodsToDisplay = foods.filter((food) =>{
+    if (filterBy === "All"){
+      return true;
+    } else{
+      return food.cuisine === filterBy;
+    }
+  });
+
+  function handleAddFood(id){
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+    setFoods(prevVal => [...prevVal, newFood])
+  } 
+  function handleClick(id){
+    setFoods(prevVal => {
+      //prevVal.filter(food => food.id !=id)
+      let newArr = prevVal.map(food =>{
+        if (food.id === id ){
+          return { ...food, heatLevel: food.heatLevel + 1}
+        }
+        else{
+          return food;
+        }
+      });
+      return newArr;
+    })
   }
 
+  function handleClick(id){
+    const newFoodArray = foods.map((food) =>{
+      if (food.id === id){
+        return{
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else{
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+  [1, 2, 3].filter((number) => number !==3);
+  [1, 2, 3].map((number) =>{
+    if (number === 3){
+      //if its the number we're looking for
+      return number + 100;
+    } else{
+      //else, return original number
+      return number;
+    }
+  });
+
+  //{
+    //const newFood = getNewRandomSpicyFood();
+    //console.log(newFood);
+    //setFoods(prevVal => [...prevVal, newFood])
+  //}
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
   return (
     <div>
+      <select name="filter">
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
